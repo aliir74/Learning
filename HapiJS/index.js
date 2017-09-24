26bb95cd761e861138b371abed18c4c244ec13ad
aliir74 <aliirani74@gmail.com>
2017-09-24 14:24:36 +0330

    add routes.js

--- HapiJS/index.js
+++ HapiJS/index.js
@@ -2,12 +2,13 @@
 
 const Hapi = require('hapi')
 const db = require('./database').db
+const routes = require('./routes')
 
 const server = new Hapi.Server()
 
 server.connection({port: 3001, host: 'localhost'})
 
-
+server.route(routes)
 
 server.start( (err) => {
     if (err) {
--- HapiJS/routes.js
+++ HapiJS/routes.js
@@ -17,9 +17,31 @@ var routes = [
     }
 }, {
         method: 'GET',
-        path: '/',
+        path: '/wolves',
         handler: function (req, res) {
-
+            Wolf.find(function (err, wolves) {
+                if (err) {
+                    console.error(err)
+                }
+                
+                res(wolves)
+            })
+        }
+    },
+    {
+        method: 'POST',
+        path: '/wolves/{name}',
+        handler: function (req, res) {
+            const wolf = new Wolf({
+                name: encodeURIComponent(req.params.name)
+            })
+            wolf.save(function (err, wolf) {
+                if (err) {
+                    console.error(err)
+                }
+                res(wolf._id)
+                
+            })
         }
     }
 ]
