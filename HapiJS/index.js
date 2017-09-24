cd8be9ed06be213083de0cec153f167b9250b2c0
aliir74 <aliirani74@gmail.com>
2017-09-24 15:16:14 +0330

    add basic authentication

--- HapiJS/index.js
+++ HapiJS/index.js
@@ -3,16 +3,51 @@
 const Hapi = require('hapi')
 const db = require('./database').db
 const routes = require('./routes')
+const Basic = require('hapi-auth-basic')
+const Bcrypt = require('bcrypt')
+const User = require('./models/User')
 
 const server = new Hapi.Server()
 
 server.connection({port: 3001, host: 'localhost'})
 
-server.route(routes)
+const validate = function (req, username, password, callback) {
+    User.find({username: username}, function (err, user) {
+        if (!user) {
+            return callback(null, false)
+        }
+        Bcrypt.compare(password, user.password, (err, isValid) => {
+            callbak(err, isValid, {id: user._id, username: user.username})
+        })
+    })
+}
 
-server.start( (err) => {
+server.register(Basic, (err) => {
     if (err) {
         throw err
     }
-    console.log(`Server running at: ${server.info.uri}`)
+
+    server.auth.strategy('simple', 'basic', {validateFunc: validate})
+
+    //server.route(routes)
+    server.route({
+        method: 'GET',
+        path: '/',
+        config: {
+            auth: 'simple',
+            handler: function (request, reply) {
+                reply('hello, ' + request.auth.credentials.name);
+            }
+        }
+    });
+
+    server.start( (err) => {
+        if (err) {
+            throw err
+        }
+        console.log(`Server running at: ${server.info.uri}`)
+    })
 })
+
+
+
