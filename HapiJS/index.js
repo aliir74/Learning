9ef8260091df65898c3737c7a9352e5f015fcd79
aliir74 <aliirani74@gmail.com>
2017-09-24 13:44:45 +0330

    add routes

--- HapiJS/index.js
+++ HapiJS/index.js
@@ -6,6 +6,22 @@ const server = new Hapi.Server()
 
 server.connection({port: 3000, host: 'localhost'})
 
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (req, res) {
+        res('hello world!')
+    }
+})
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (req, res) {
+        res('hello ' + encodeURIComponent(req.params.name))
+    }
+})
+
 server.start((err) => {
     if (err) {
         throw err
