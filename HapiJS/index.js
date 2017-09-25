cb8f11c9e0e04ff354685494e5c6c4a1d18f7dd7
aliir74 <aliirani74@gmail.com>
2017-09-25 18:50:55 +0330

    basic auth work correctly

--- HapiJS/index.js
+++ HapiJS/index.js
@@ -6,20 +6,27 @@ const routes = require('./routes')
 const Basic = require('hapi-auth-basic')
 const Bcrypt = require('bcrypt')
 const User = require('./models/User')
+const Wolf = require('./models/Wolf')
 
 const server = new Hapi.Server()
 
-server.connection({port: 3001, host: 'localhost'})
+server.connection({port: 8000, host: 'localhost'})
 
 const validate = function (req, username, password, callback) {
-    User.find({username: username}, function (err, user) {
-        if (!user) {
+    Wolf.find({name: username}, function (err, user) {
+        if (user.length === 0) {
             return callback(null, false)
         }
-        Bcrypt.compare(password, user.password, (err, isValid) => {
-            callbak(err, isValid, {id: user._id, username: user.username})
+        console.log(user)
+        callback(err, true, {id: user[0]._id, username: user[0].name})
+        /*
+        Bcrypt.compare(password, password, (err, isValid) => {
+            console.log(isValid)
+            callback(err, isValid, {id: user._id, username: user.name})
         })
+        */
     })
+
 }
 
 server.register(Basic, (err) => {
@@ -36,7 +43,7 @@ server.register(Basic, (err) => {
         config: {
             auth: 'simple',
             handler: function (request, reply) {
-                reply('hello, ' + request.auth.credentials.name);
+                reply('hello, ' + request.auth.credentials.username);
             }
         }
     });
