6ec5b882cd1663cae9c895ad71a027cbec11e125
aliir74 <aliirani74@gmail.com>
2018-01-25 20:38:00 +0330

    change default admin template (just change title of admin site)

--- Django/polls/admin.py
+++ Django/polls/admin.py
@@ -2,6 +2,25 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+#admin.site.register(Question)
+
+class ChioceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,  {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChioceInline]
+
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
+
+admin.site.register(Choice)
